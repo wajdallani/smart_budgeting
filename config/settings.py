07fc77fb641b7bf22a28cc9a92df880add21e43f
baraa131313 <baraa.zouaghi@esprit.tn>
2025-11-29 15:17:44 +0100

    updated media

--- config/settings.py
+++ config/settings.py
@@ -166,6 +166,9 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 STATICFILES_DIRS = [
     BASE_DIR / "src" / "assets",
