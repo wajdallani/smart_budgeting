83970fc8652fb4c6c6e3ac4155838b120ee17c1a
Safa Ben Haddad <Safa.BEN-HADDAD@esprit.tn>
2025-12-13 15:10:32 +0100

    email credentials removed

--- config/settings.py
+++ config/settings.py
@@ -17,6 +17,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 from .template import  THEME_LAYOUT_DIR, THEME_VARIABLES
+from decouple import config
 
 load_dotenv()  # take environment variables from .env.
 
@@ -212,10 +213,4 @@ CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'UTC'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sbenhaddad002@gmail.com'
-EMAIL_HOST_PASSWORD = 'siii gial gwdk hzge'
-DEFAULT_FROM_EMAIL = 'sbenhaddad002@gmail.com'
+
